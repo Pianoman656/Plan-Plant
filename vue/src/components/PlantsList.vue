f83d07103d880cb3e7873f25b56f0260f4a7cd93
Aaron Lee <r.aaron.lee@gmail.com>
2022-04-15 19:22:46 -0400

    renamed the Plants service to PlantsService service; Also added a PlantsList component

---
+++ vue/src/components/PlantsList.vue
@@ -0,0 +1,71 @@
+<template>
+  
+  <table>
+    <thead>
+      <tr>
+        <!-- <th>&nbsp;</th>  -->
+        <th>Plant Name</th>
+        <th>Square Area</th>
+        <th>Cost</th>
+        <th>Hardiness Zone</th>
+        <th>Sun Requirements</th>        
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="plant in plants" :key="plant.id" v-on:click="viewPlant(plant.id)">        
+        <td class="name">{{ plant.commonName }}</td>        
+        <td class="square-area">{{plant.squareArea}}</td>
+        <td class="cost">{{plant.cost}}</td>    
+        <td class="hardiness-zone">{{plant.hardinessZone}}</td>
+        <td class="sun-requirements">{{plant.sunRequirements}}</td>
+      </tr>
+    </tbody>  
+  </table>
+</template>
+
+
+<script>
+
+import plants from '../services/PlantsService'
+
+export default {
+
+    name: "plants-list",
+    data() {
+        return {
+        plants: []
+        };
+    },
+    methods: {
+        viewPlant(id) {
+        this.$router.push(`/plant/${id}`);
+        }
+    },
+    created() {
+        plants.list().then((response) => {
+        this.plants = response.data;
+        });
+   }
+    
+}
+</script>
+
+<style>
+
+
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin: 0;
+  padding: 0;
+}
+
+th {
+  font-family: "Work Sans", sans-serif;
+  font-weight: 500;
+  text-align: left;
+}
+
+
+</style>
--- vue/src/services/PlantsService.js
+++ vue/src/services/PlantsService.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 
 export default {
     list() {
-      return axios.get('/plants/');
+      return axios.get('/plant/');
     },
   
     get(id) {
-      return axios.get(`/plants/${id}`)
+      return axios.get(`/plant/${id}`)
     }  
   }
