254625b9c45e61564b17d72679e0589eb5a89728
pianoman656 <jhille888@gmail.com>
2022-04-21 19:18:09 -0400

    CalculatorWorking

--- vue/src/components/ShoppingList.vue
+++ vue/src/components/ShoppingList.vue
@@ -2,17 +2,21 @@
 <div id="all-page">
   <h1>Supplies for your Garden</h1>
   <div id="supply-page">
+        
+        
         <div class="container" v-for="supply in myShoppingList" :key="supply.supplyId">
           <a href="https://www.ruralking.com"> 
             <img :src="supply.imageUrl" class="supply-image"/>
           </a>
            <div> 
-            <h2>{{ supply.supplyName }}</h2>
+            <h2>{{supply.supplyName}}</h2>
             <p>{{supply.description}}</p>
             <h2>${{supply.supplyCost}}</h2>
-            <button @click='addToCart(product)' class='button is-info'>Remove from Cart</button> 
+            <button @click='removeFromList(supply)' class='button is-info'>Remove from List</button> 
            </div>
         </div>
+        
+        
         <div class="container" v-for="plant in sortedPlants" :key="plant.plantId">
           <a href="https://www.ufseeds.com"> 
             <img :src="plant.imageUrl" class="plant-image"/>
@@ -21,10 +25,11 @@
             <h2>{{plant.commonName}}</h2>
             <p>{{plant.description}}</p>
             <h2>${{plant.cost}}</h2>
-            <button @click='addToCart(product)' class='button is-info'>Remove from cart</button>
+            <button @click='removeFromList(product)' class='removeFromList'>Remove from list</button>
            </div>
         </div>
-  </div>
+      <h2 class="cost">Estimated cost of your farm supplies: ${{totalCost}}</h2>
+    </div>
   </div>
 </template>
 
@@ -36,6 +41,7 @@ import plotsService from '../services/PlotsService'
 
 export default {
     name: 'shopping-list',
+    
     methods: {
         getTotalAreaOfFarm() {
           plotsService.listPlots().then(response => {
@@ -47,7 +53,7 @@ export default {
             this.$store.commit("SET_SUPPLIES", response.data);
             });
         },
-        calculateTotal() {
+        removeFromList(){
 
         }
     },
@@ -66,22 +72,36 @@ export default {
             this.$store.commit("SET_PLOTS", response.data);
         });
     },
+
     computed: {
         myShoppingList() {
           return this.$store.state.shoppingList.filter(() => {
               return true;
           });
         },
-        expectedCost() {
-          return this.$store.state.plants.filter((plant) => {
-              return plant; // 
-          });
-        },
+        totalCost() {
+          let total = 0;
+          let list = this.$store.state.shoppingList;
+          for(let i = 0; i < list.length; i++)
+          {
+            total += list[i].supplyCost;
+          }
+          return total;
+        }
     }    
 }
 </script>
 
 <style scoped>
+h1{
+  color:black;
+}
+
+h2.cost{
+  color:white;
+  background-color:black;
+  font-size: 50px
+}
 .container {
     color: ivory;
     text-shadow: gray ;
